4e49622812ee78d4a896b0543492d96ff18b8eb2
Tristan Behrens <tristan.behrens@mhp.com>
2016-09-29 15:44:13 +0200

    Adds unit tests. One for success. Another for failure.

--- PCICSampleAppUITests/PCICSampleAppUITests.swift
+++ PCICSampleAppUITests/PCICSampleAppUITests.swift
@@ -28,9 +28,12 @@ class PCICSampleAppUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testExampleSuccess() {
+        XCTAssertTrue(2 + 2 == 4, "Should succeed.")
     }
     
+    func testExampleFailure() {
+        XCTAssertTrue(2 + 3 == 6, "Should fail.")
+    }
+
 }
